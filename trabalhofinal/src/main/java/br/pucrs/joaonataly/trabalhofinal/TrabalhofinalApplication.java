a76cc3007a5a74d4e442bfa2e59f08336ce974f4
Jhanos <joao.sbardelotto@edu.pucrs.br>
2025-06-03 22:06:24 -0300

    Springboot maven corretos agora

---
+++ trabalhofinal/src/main/java/br/pucrs/joaonataly/trabalhofinal/TrabalhofinalApplication.java
@@ -0,0 +1,13 @@
+package br.pucrs.joaonataly.trabalhofinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhofinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhofinalApplication.class, args);
+	}
+
+}
---
+++ trabalhofinal/src/test/java/br/pucrs/joaonataly/trabalhofinal/TrabalhofinalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.pucrs.joaonataly.trabalhofinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhofinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
