f6c7ed1bd89fb2d78d5a4871d8bffc522a75d892
Jhanos <joao.sbardelotto@edu.pucrs.br>
2025-06-24 16:16:33 -0300

    classes restantes

--- trabalhofinal/src/main/java/br/pucrs/joaonataly/trabalhofinal/application/dtos/JogoRequestDTO.java
+++ trabalhofinal/src/main/java/br/pucrs/joaonataly/trabalhofinal/application/dtos/JogoRequestDTO.java
@@ -1,21 +1,32 @@
 package br.pucrs.joaonataly.trabalhofinal.application.dtos;
 
+import br.pucrs.joaonataly.trabalhofinal.domain.model.JogoEletronicoModel;
+import br.pucrs.joaonataly.trabalhofinal.domain.model.JogoMesaModel;
+import br.pucrs.joaonataly.trabalhofinal.domain.model.JogoModel;
+import br.pucrs.joaonataly.trabalhofinal.domain.model.ENUM.TipoEletronicoModel;
+import br.pucrs.joaonataly.trabalhofinal.domain.model.ENUM.TipoMesaModel;
+
 public class JogoRequestDTO {
-    private String tipoJogo;
 
     private String nome;
     private double valorBase;
 
     // Campos para Jogo Eletrônico
     private String plataforma;
-    private String tipoEletronico;
+    private TipoEletronicoModel tipoEletronico;
+    
 
     // Campos para Jogo de Mesa
     private Integer numeroPecas;
-    private String tipoMesa;
+    private TipoMesaModel tipoMesa;
 
-    public String getTipoJogo() {
-        return tipoJogo;
+    public JogoRequestDTO(String nome, double valorBase, String plataforma, TipoEletronicoModel tipoEletronico, Integer numeroPecas, TipoMesaModel tipoMesa) {
+        this.nome = nome;
+        this.valorBase = valorBase;
+        this.plataforma = plataforma;
+        this.tipoEletronico = tipoEletronico;
+        this.numeroPecas = numeroPecas;
+        this.tipoMesa = tipoMesa;
     }
 
     public String getNome() {
@@ -30,7 +41,7 @@ public class JogoRequestDTO {
         return plataforma;
     }
 
-    public String getTipoEletronico() {
+    public TipoEletronicoModel getTipoEletronico() {
         return tipoEletronico;
     }
 
@@ -38,7 +49,18 @@ public class JogoRequestDTO {
         return numeroPecas;
     }
 
-    public String getTipoMesa() {
+    public TipoMesaModel getTipoMesa() {
         return tipoMesa;
     }
+
+    public String getTipoJogo() {
+        if (tipoEletronico != null) {
+            return "ELETRONICO";
+        } else if (numeroPecas != null && tipoMesa != null) {
+            return "MESA";
+        } else {
+            throw new IllegalArgumentException("Tipo de jogo não especificado corretamente.");
+        }
+    }
+    
 }
