2f490eaa373b5eca82b36d60ca3bc7c00f8e011e
Jhanos <joaocsbardelotto@gmail.com>
2025-06-24 21:27:05 +0000

    FUNCIONANDO!!!

--- trabalhofinal/src/main/java/br/pucrs/joaonataly/trabalhofinal/TrabalhofinalApplication.java
+++ trabalhofinal/src/main/java/br/pucrs/joaonataly/trabalhofinal/TrabalhofinalApplication.java
@@ -3,11 +3,12 @@ package br.pucrs.joaonataly.trabalhofinal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "br.pucrs.joaonataly.trabalhofinal")
 public class TrabalhofinalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TrabalhofinalApplication.class, args);
+		System.out.println("=== Servidor iniciado com sucesso ===");
 	}
 
 }
